d81e35a543c8f8f5167cf79873a0af00f86c633d
Alex Barten <mail@alexbarten.com>
2022-10-01 20:36:08 +0200

    Change Roman numerals to uppercase

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -1,10 +1,9 @@
 class RomanNumeralTranslator:
     def translate(self, roman_number):
-        roman_number_table = {'i': 1, 'v': 5, 'x': 10, 'l': 50, 'c': 100,
-                              'd': 500, 'm': 1000}
+        roman_number_table = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                              'D': 500, 'M': 1000}
 
         return roman_number_table.get(roman_number)
-
 
     def calculate_multi_numbers(self, roman_number):
         translation_list = []
--- tests/test_roman_numerals.py
+++ tests/test_roman_numerals.py
@@ -6,56 +6,56 @@ from roman_numerals.roman_numerals import RomanNumeralTranslator
 def test_translate_i():
     translator = RomanNumeralTranslator()
 
-    result = translator.translate('i')
+    result = translator.translate('I')
     assert result == 1
 
 
 def test_translate_v():
     translator = RomanNumeralTranslator()
 
-    result = translator.translate('v')
+    result = translator.translate('V')
     assert result == 5
 
 
 def test_translate_x():
     translator = RomanNumeralTranslator()
 
-    result = translator.translate('x')
+    result = translator.translate('X')
     assert result == 10
 
 
 def test_translate_l():
     translator = RomanNumeralTranslator()
 
-    result = translator.translate('l')
+    result = translator.translate('L')
     assert result == 50
 
 
 def test_translate_c():
     translator = RomanNumeralTranslator()
 
-    result = translator.translate('c')
+    result = translator.translate('C')
     assert result == 100
 
 
 def test_translate_d():
     translator = RomanNumeralTranslator()
 
-    result = translator.translate('d')
+    result = translator.translate('D')
     assert result == 500
 
 
 def test_translate_m():
     translator = RomanNumeralTranslator()
 
-    result = translator.translate('m')
+    result = translator.translate('M')
     assert result == 1000
 
 
 def test_validate_each_numeral():
     translator = RomanNumeralTranslator()
 
-    result = translator.translate('j')
+    result = translator.translate('J')
     assert result is None
 
 
@@ -64,19 +64,19 @@ def test_transform_multi_numbers():
     # and calculate them
     translator = RomanNumeralTranslator()
 
-    result = translator.calculate_multi_numbers('ii')
+    result = translator.calculate_multi_numbers('II')
     assert result == 2
 
-    result = translator.calculate_multi_numbers('xx')
+    result = translator.calculate_multi_numbers('XX')
     assert result == 20
 
-    result = translator.calculate_multi_numbers('dx')
+    result = translator.calculate_multi_numbers('DX')
     assert result == 510
 
-    result = translator.calculate_multi_numbers('mdclxvi')
+    result = translator.calculate_multi_numbers('MDCLXVI')
     assert result == 1666
 
 # TODO: Add exception handling: illegal characters, illegal combinations
-#       like 'vv', 'il'.
+#       like 'VV', 'IL'.
 # TODO: Add handling of smaller numerals that make subsequent larger
-#       numeral smaller (like iv, ix, cm).
+#       numeral smaller (like IV, IX, XM).
