2f7f8e29f7cb6fc5199de7c0acd77d1d7b134037
Alex Barten <mail@alexbarten.com>
2022-10-01 17:41:54 +0200

    Refactor number translation to dictionary-lookup

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -1,22 +1,9 @@
 class RomanNumeralTranslator:
     def translate(self, roman_number):
-        answer = None
-        roman_number_table = {'i': 1}
-        if roman_number == 'i':
-            answer = roman_number_table.get('i')
-        elif roman_number == 'v':
-            answer = 5
-        elif roman_number == 'x':
-            answer = 10
-        elif roman_number == 'l':
-            answer = 50
-        elif roman_number == 'c':
-            answer = 100
-        elif roman_number == 'd':
-            answer = 500
-        elif roman_number == 'm':
-            answer = 1000
-        return answer
+        roman_number_table = {'i': 1, 'v': 5, 'x': 10, 'l': 50, 'c': 100,
+                              'd': 500, 'm': 1000}
+
+        return roman_number_table.get(roman_number)
 
     def calculate_multi_numbers(self, roman_number):
         translation_list = []
