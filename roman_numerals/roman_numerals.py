85fa1f36f90ff85fead91c92b7cc18d1d4f9e4ac
Alex Barten <mail@alexbarten.com>
2022-10-14 10:15:21 +0200

    Add docstrings and sanitise use of _numeral_ instead of _number_

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -1,26 +1,37 @@
 class RomanNumeralTranslator:
-    """ Translate Roman numbers into Arabic numbers.
+    """ Translate Roman numerals into Arabic numbers.
 
-    This module is used to translate Roman numbers into Arabic numbers.
+    This module is used to translate Roman numerals into Arabic numbers.
     """
 
-    def translate(self, roman_number):
+    def translate(self, roman_numeral):
         """Translate a single Roman numeral into an Arabic number.
 
         Args:
-            roman_number (string): a single roman numeral
+            roman_numeral (string): a single roman numeral
 
         Returns:
             int: Arabic number - representing the value of the Roman
             numeral
         """
 
-        roman_number_table = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-                              'D': 500, 'M': 1000}
+        roman_numeral_table = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                               'D': 500, 'M': 1000}
 
-        return roman_number_table.get(roman_number.upper())
+        return roman_numeral_table.get(roman_numeral.upper())
 
     def validate_multinumerals(self, multinumeral):
+        """Check if multinumeral is a valid Roman numeral.
+
+        Check for invalid two-numeral combinations in a Roman numeral.
+
+        Args:
+            multinumeral (string): Roman numeral (single or multinumeral)
+
+        Returns:
+            boolean: True if valid, else False.
+        """
+
         invalid_doubles = ['vv', 'll', 'dd']
         if any(i in multinumeral for i in invalid_doubles):
             return False
@@ -28,6 +39,19 @@ class RomanNumeralTranslator:
             return True
 
     def calculate_numeral(self, numeral):
+        """Calculate the value of a Roman numeral.
+
+        Calculate the Arabic number that represents a Roman single or
+        multinumeral.
+
+        Args:
+            numeral (string): A Roman single or multinumeral.
+
+        Returns:
+            int: Arabic number representing the Roman numeral,
+            or None when translation was not possible.
+        """
+
         translation_list = []
         for symbol in numeral:
             translation_list.append(self.translate(symbol))
