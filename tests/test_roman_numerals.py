5e9193f1f0142280ed124c14cdbb443d4a0b8adb
Alex Barten <mail@alexbarten.com>
2022-10-17 21:20:11 +0200

    Parametrise single numeral tests.

--- tests/test_roman_numerals.py
+++ tests/test_roman_numerals.py
@@ -1,55 +1,21 @@
+import pytest
+
 from roman_numerals.roman_numerals import RomanNumeralTranslator
 
-# test all single roman numeral symbols
 
-
-def test_translate_i():
+@pytest.mark.parametrize("numeral, arabic",
+                         [('I', 1),
+                          ('V', 5),
+                          ('X', 10),
+                          ('L', 50),
+                          ('C', 100),
+                          ('D', 500),
+                          ('M', 1000)])
+def test_translate_single_numeral(numeral, arabic):
     translator = RomanNumeralTranslator()
 
-    result = translator._translate('I')
-    assert result == 1
-
-
-def test_translate_v():
-    translator = RomanNumeralTranslator()
-
-    result = translator._translate('V')
-    assert result == 5
-
-
-def test_translate_x():
-    translator = RomanNumeralTranslator()
-
-    result = translator._translate('X')
-    assert result == 10
-
-
-def test_translate_l():
-    translator = RomanNumeralTranslator()
-
-    result = translator._translate('L')
-    assert result == 50
-
-
-def test_translate_c():
-    translator = RomanNumeralTranslator()
-
-    result = translator._translate('C')
-    assert result == 100
-
-
-def test_translate_d():
-    translator = RomanNumeralTranslator()
-
-    result = translator._translate('D')
-    assert result == 500
-
-
-def test_translate_m():
-    translator = RomanNumeralTranslator()
-
-    result = translator._translate('M')
-    assert result == 1000
+    result = translator._translate(numeral)
+    assert result == arabic
 
 
 def test_validate_each_numeral():
